d0d0f9d32b405a0185d7f911f9a5df29594ef9bd
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-07-16 15:10:08 +0530

    Added:uc2choice of move for a player

--- src/com/timbhuchalka/Tictak.java
+++ src/com/timbhuchalka/Tictak.java
@@ -1,20 +1,38 @@
 package com.timbhuchalka;
+
+import java.util.Scanner;
+
 //create class
 public class Tictak {
+    static char[] board= new char[10];
+    static char shiftchoice;
 //create methode
     public static char[] createboard(){
 //creatin array with size 10
-        char[] board= new char[10];
+
         for(int i=0;i<board.length;i++) {
             board[i]=' ';
         }
             return board;
     }
+    //methode to take user input
+    public static char playeroption()
+    {
+      Scanner sc=new Scanner(System.in);
+        System.out.println("enter a shift choice");
+        shiftchoice=sc.next().charAt(0);
+        System.out.println("your choice - " +shiftchoice);
+        return shiftchoice;
+    }
             //main methode
         public static void main(String[] args) {
         //calling methode
            char[] board =  createboard();
-
+           //taking input for player and computer
+            System.out.println("enter option");
+            char shiftchoice=playeroption();
+            System.out.println("enter option");
+            char computerchice=playeroption();
         }
     }
 
